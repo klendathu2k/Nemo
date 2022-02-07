c8f1cea6a617bb10f23f69632ab3fab951840fae
Jason C. Webb <jwebb@bnl.gov>
2022-02-07 11:43:13 -0500

    Change to dataclass for inputset / fix typo in init

--- Nemo/Inputset.py
+++ Nemo/Inputset.py
@@ -1,15 +1,43 @@
+from dataclasses import dataclass
+
+import uuid
+
+@dataclass ( frozen = True )
 class Inputset:
-    def __init__(self):
-        self.local_inputs  = []
-        self.remote_inputs = []
-        self.transfer
+    name      :   str
+    unique_id   : str
+    files       : list[str]
+    status      : list[str]
+    source      : str
+    destination : str
+    
+    
+
+#class Inputset:
+#    def __init__(self):
+#        self.local_inputs  = []
+#        self.remote_inputs = []
+#        self.transfer
 
 class InputsetBuilder:
-    def __init(self):
-        self.product = None
-        self.origin           = ""
-        self.destination      = ""
-        self.transfer_command = ""
-        self.inputs = []        
+    def __init__(self):
+        self.name        = ""
+        self.product     = None
+        self.files       = []
+        self.status      = []
+        self.source      = ""
+        self.destination = ""
+
+    def build(self):
+        self.product=Inputset(
+            name        = self.name,
+            unique_id   = str(uuid.uuid4()),
+            files       = self.files,
+            source      = self.source,
+            destination = self.destination,
+            status      = [None] * len(self.files)
+            )
+        return self.product
+
         
     
